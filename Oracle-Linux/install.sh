424bd406e1deb570662041f31d48624613cb10ee
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-06-18 20:30:02 +0100

    Update install.sh

--- Oracle-Linux/install.sh
+++ Oracle-Linux/install.sh
@@ -13,3 +13,6 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
+
+
+# sudo lvextend -l +100%FREE /dev/mapper/ocivolume-root
